da03cc54f89e2faef8f8f1f5db75ec99b1dacdeb
Raymond Julin <raymond@keyteq.no>
2011-03-26 04:26:25 -0400

    Updated twig layout

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -29,6 +29,7 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
     <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
+
 	<?php echo $this->scripts(); ?>
 
     <!--[if lt IE 7 ]>
