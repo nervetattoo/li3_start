6b1bc2951b41bffc36125dbf929ba14e92ae882c
Raymond Julin <raymond@keyteq.no>
2011-03-26 06:45:01 -0400

    Totaly bare bone backbone app controller

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,4 +1,31 @@
 /**
  * Main App controller.
- * Pack your default stuff in here as a `Backbone.Controller`
+ * Controlls routing and delegating to the real views
  */
+AppController = Backbone.Controller.extend({
+
+    el : $("#container"),
+
+    initialize : function(options) {
+        _.bindAll(this, "home");
+
+        /**
+         * When a user enters without a hash in url (/)
+         * pass over to #send. This kickstarts the send view
+         */
+        var hash = window.location.hash;
+        if (hash == "" || hash == "#")
+            window.location.hash = "home";
+    },
+
+    routes : {
+        "home" : "home"
+    },
+
+    /**
+     * Render the default start page of app
+     * You most likely want to utilize a `Backbone.View` for all routes
+     */
+    home : function() {
+    }
+});
